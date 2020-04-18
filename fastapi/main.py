5784c9e9cc7c87d8124c26a16852d56963595eeb
yu-ya4 <yuya4.matsumura@gmail.com>
2020-04-18 23:23:10 +0900

    Add PUT

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,7 +1,14 @@
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
 
 
 @app.get("/")
@@ -12,3 +19,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
