b43f866da416b09f9fd7d8c41254508caa4e2ad7
yu-ya4 <yuya4.matsumura@gmail.com>
2020-06-12 10:47:26 +0900

    Add static path

--- django-sample/manager_project/manager_project/settings.py
+++ django-sample/manager_project/manager_project/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Static file settings
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
